5028a835e1a6b8d02352179f7ecef0f88812d875
AjinkyaVeer <veerajinkya1@gmail.com>
2023-01-08 22:33:02 +0530

    changes in login page

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -94,19 +94,6 @@ function Login() {
               </div>
 
               <div className="flex justify-between items-center mb-6">
-                <div className="form-group form-check">
-                  <input
-                    type="checkbox"
-                    className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                    id="exampleCheck2"
-                  />
-                  <label
-                    className="form-check-label inline-block text-gray-800"
-                    for="exampleCheck2"
-                  >
-                    Remember me
-                  </label>
-                </div>
                 <a href="#!" className="text-gray-800">
                   Forgot password?
                 </a>
@@ -123,7 +110,7 @@ function Login() {
                   Don't have an account?
                   <a
                     href="#!"
-                    className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                    className="ml-2 text-blue-600 transition duration-200 ease-in-out"
                   >
                     Register
                   </a>
