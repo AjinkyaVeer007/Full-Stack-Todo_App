5ecf7215b17d579810594c16e74e351daee87eba
AjinkyaVeer <veerajinkya1@gmail.com>
2023-01-11 21:08:20 +0530

    backend stuffs added

---
+++ backend/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+exports.connect = () => {
+  mongoose
+    .connect(MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("DB Connected Successfully"))
+    .catch((err) => {
+      console.log("DB Connection failed");
+      console.log(err);
+      process.exit(1);
+    });
+};
---
+++ backend/model/user.login.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userLoginSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userLoginSchema);
